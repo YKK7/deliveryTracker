0a9968e024df2ad44da819442d2894b0b1b3f75b
Chris <chriskim80@gmail.com>
2017-06-27 18:16:43 -0400

    Added methods to center map at the average of driver locations and to grab the first name only for markers

--- ionic/src/providers/user-service.ts
+++ ionic/src/providers/user-service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, Response, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs';
@@ -9,10 +9,29 @@ export class UserService {
   public API = 'http://localhost:8080';
   public USER_API = this.API + '/users';
 
-  constructor(public http: Http) {}
+  constructor(public http: Http) { }
 
   getUsers(): Observable<any> {
     return this.http.get(this.USER_API)
       .map((response: Response) => response.json());
+  }
+
+  create(user: any): Observable<any> {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post("http://localhost:8080/users/", JSON.stringify(user), { headers: headers });
+  }
+
+  delete(user: any): Observable<any> {
+    let DELETE_THIS = "http://localhost:8080/users/" + user.id;
+    return this.http.delete(DELETE_THIS);
+  }
+
+  getUser(id: any): Observable<any> {
+    return this.http.get("http://localhost:8080/users/" + id).map(response => response.json());
+  }
+
+  update(user: any): Observable<any> {
+    return this.http.put("http://localhost:8080/users/", user).map(response => response.json());
   }
 }
